5ed7f2726b64fc52f3ca2ddf9d0b3732c3d9c163
DimitrisPyrkas <dimitrispyrkas@gmail.com>
2025-06-12 18:06:49 +0300

    Initial commi

--- MyProjects/Library/src/main/java/com/example/Library/Entity/Role.java
+++
@@ -1,8 +0,0 @@
-package com.example.Library.Entity;
-
-//Enum representing the role a user can have in the library system
-
-public enum Role {
-    ADMIN,//Has full access (add/update/delete books, view all transactions)
-    MEMBER//Can borrow/return books and view personal transactions
-}
